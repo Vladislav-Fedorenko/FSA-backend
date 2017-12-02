8c748f3488f8c180c14898fdb13cb2a91b614662
Vladislav Fedorenko <fedorenko.vladislav.k@gmail.com>
2017-12-02 17:21:01 +0600

    Renamed upsertExecutor to updateExecutor. Created selectByIdExecutor

--- src/main/java/topLevelDomain/secondLevelDomain/homeneeds/utils/database/impementation/SelectByIdExecutor.java
+++ src/main/java/topLevelDomain/secondLevelDomain/homeneeds/utils/database/impementation/SelectByIdExecutor.java
@@ -3,7 +3,7 @@ package topLevelDomain.secondLevelDomain.homeneeds.utils.database.impementation;
 import topLevelDomain.secondLevelDomain.homeneeds.utils.database.DatabaseTasksExecutor;
 import topLevelDomain.secondLevelDomain.homeneeds.utils.database.execption.DatabaseTasksExecutorException;
 
-public class UpsertExecutor implements DatabaseTasksExecutor{
+public class SelectByIdExecutor implements DatabaseTasksExecutor{
   @Override
   public void execute() throws DatabaseTasksExecutorException {
 

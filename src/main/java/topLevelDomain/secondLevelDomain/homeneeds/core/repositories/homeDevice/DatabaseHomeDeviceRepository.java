b60e25c9e18c48abc5ef45efd349f97b3c560978
Vladislav Fedorenko <fedorenko.vladislav.k@gmail.com>
2017-12-09 16:51:52 +0600

    Added implementation of method addNewHomeDevice

--- src/main/java/topLevelDomain/secondLevelDomain/homeneeds/core/repositories/homeDevice/DatabaseHomeDeviceRepository.java
+++ src/main/java/topLevelDomain/secondLevelDomain/homeneeds/core/repositories/homeDevice/DatabaseHomeDeviceRepository.java
@@ -1,13 +1,29 @@
 package topLevelDomain.secondLevelDomain.homeneeds.core.repositories.homeDevice;
 
 import topLevelDomain.secondLevelDomain.homeneeds.core.models.HomeDevice;
+import topLevelDomain.secondLevelDomain.homeneeds.utils.database.DatabaseTasksExecutorFactory;
+import topLevelDomain.secondLevelDomain.homeneeds.utils.database.InsertExecutor;
+import topLevelDomain.secondLevelDomain.homeneeds.utils.database.exeception.DatabaseTasksExecutorException;
 
 import java.util.List;
 
 public class DatabaseHomeDeviceRepository implements HomeDeviceRepository {
+  private InsertExecutor<HomeDevice> insertExecutor;
+
+  @SuppressWarnings("unchecked")
+  public DatabaseHomeDeviceRepository(DatabaseTasksExecutorFactory factory) {
+    insertExecutor = factory.getInsertExecutor();
+  }
 
   @Override
   public HomeDevice addNewHomeDevice(HomeDevice homeDevice) {
+    try {
+      insertExecutor.setInsertingObject(homeDevice);
+      insertExecutor.execute();
+      return insertExecutor.getResult();
+    } catch (DatabaseTasksExecutorException e) {
+      e.printStackTrace();
+    }
     return null;
   }
 

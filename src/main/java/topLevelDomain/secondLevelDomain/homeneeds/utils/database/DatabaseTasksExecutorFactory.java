28da9f12af8a9fb0187940eba44ffc3e91e3bcbf
Vladislav Fedorenko <fedorenko.vladislav.k@gmail.com>
2017-12-09 16:51:03 +0600

    Added methods to factory of databaseTasksExecutor

--- src/main/java/topLevelDomain/secondLevelDomain/homeneeds/utils/database/DatabaseTasksExecutorFactory.java
+++ src/main/java/topLevelDomain/secondLevelDomain/homeneeds/utils/database/DatabaseTasksExecutorFactory.java
@@ -1,4 +1,31 @@
 package topLevelDomain.secondLevelDomain.homeneeds.utils.database;
 
+import topLevelDomain.secondLevelDomain.homeneeds.utils.database.impementation.InsertExecutorImpl;
+
 public class DatabaseTasksExecutorFactory {
+  private SelectByIdExecutor selectByIdExecutor;
+  private SelectExecutor selectExecutor;
+  private InsertExecutor insertExecutor;
+  private UpdateExecutor updateExecutor;
+  private DeleteExecutor deleteExecutor;
+
+  public SelectByIdExecutor getSelectByIdExecutor() {
+    return selectByIdExecutor;
+  }
+
+  public SelectExecutor getSelectExecutor() {
+    return selectExecutor;
+  }
+
+  public InsertExecutor getInsertExecutor() {
+    return new InsertExecutorImpl<>();
+  }
+
+  public UpdateExecutor getUpdateExecutor() {
+    return updateExecutor;
+  }
+
+  public DeleteExecutor getDeleteExecutor() {
+    return deleteExecutor;
+  }
 }

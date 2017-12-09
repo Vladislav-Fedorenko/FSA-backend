bdadf0421a62f15013f02a120ef4e8e78335e1a2
Vladislav Fedorenko <fedorenko.vladislav.k@gmail.com>
2017-12-09 23:14:38 +0600

    Added constructor and changed method execute in InsertExecutorImpl

--- src/main/java/topLevelDomain/secondLevelDomain/homeneeds/utils/database/impementation/InsertExecutorImpl.java
+++ src/main/java/topLevelDomain/secondLevelDomain/homeneeds/utils/database/impementation/InsertExecutorImpl.java
@@ -2,31 +2,40 @@ package topLevelDomain.secondLevelDomain.homeneeds.utils.database.impementation;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import topLevelDomain.secondLevelDomain.homeneeds.utils.database.extending.InsertExecutor;
 import topLevelDomain.secondLevelDomain.homeneeds.utils.database.exeception.DatabaseTasksExecutorException;
+import topLevelDomain.secondLevelDomain.homeneeds.utils.timestamp.extending.EntityTimestamp;
 
-public class InsertExecutorImpl<T> implements InsertExecutor<T> {
-  @Autowired
+import java.sql.Timestamp;
+import java.time.Instant;
+
+public class InsertExecutorImpl<T extends EntityTimestamp> implements InsertExecutor<T> {
   private SessionFactory sessionFactory;
   private T insertingObject;
   private T resultInserting;
 
+  public InsertExecutorImpl(SessionFactory sessionFactory) {
+    this.sessionFactory = sessionFactory;
+  }
+
   @Override
   public void execute() throws DatabaseTasksExecutorException {
     try {
+      Timestamp timestamp = Timestamp.from(Instant.now());
+      insertingObject.setCreatedAt(timestamp);
+      insertingObject.setUpdatedAt(timestamp);
+
       Session session = sessionFactory.openSession();
       session.beginTransaction();
       session.save(insertingObject);
       session.getTransaction().commit();
+
+      insertingObject.setCreatedAt(timestamp);
+      insertingObject.setUpdatedAt(timestamp);
       this.resultInserting = this.insertingObject;
     } catch (Exception e) {
       throw new DatabaseTasksExecutorException("", e);
     }
-  }
-
-  public void setSessionFactory(SessionFactory sessionFactory) {
-    this.sessionFactory = sessionFactory;
   }
 
   @Override

eb360379d3e770ed9f5c861e6eb60185fa1cd8b8
Vladislav Fedorenko <fedorenko.vladislav.k@gmail.com>
2017-12-09 23:17:23 +0600

    Added constructor to entity "device" and changed fields createdAt, updatedAt

--- src/main/java/topLevelDomain/secondLevelDomain/homeneeds/core/models/Device.java
+++ src/main/java/topLevelDomain/secondLevelDomain/homeneeds/core/models/Device.java
@@ -1,13 +1,16 @@
 package topLevelDomain.secondLevelDomain.homeneeds.core.models;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import topLevelDomain.secondLevelDomain.homeneeds.utils.timestamp.extending.EntityTimestamp;
+
 import javax.persistence.*;
 import java.sql.Timestamp;
 import java.util.Set;
 
 @Entity
 @Table(name = "devices")
-public class Device {
-
+public class Device implements EntityTimestamp {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
@@ -23,6 +26,18 @@ public class Device {
 
   @OneToMany(mappedBy = "device", fetch = FetchType.LAZY)
   private Set<HomeDevice> homeDevices;
+
+  @JsonCreator
+  public Device(@JsonProperty("name") String name,
+                @JsonProperty("createdAt") String createdAt,
+                @JsonProperty("updatedAt") String updatedAt) {
+    this.name = name;
+//    this.createdAt = Timestamp.from(Instant.parse(createdAt));
+    if(createdAt != null && updatedAt != null) {
+      this.createdAt = new Timestamp(Long.valueOf(createdAt));
+      this.updatedAt = new Timestamp(Long.valueOf(updatedAt));
+    }
+  }
 
   public Long getId() {
     return id;
@@ -40,27 +55,25 @@ public class Device {
     this.name = name;
   }
 
-  public Timestamp getCreatedAt() {
-    return createdAt;
-  }
-
-  public void setCreatedAt(Timestamp createdAt) {
-    this.createdAt = createdAt;
-  }
-
-  public Timestamp getUpdatedAt() {
-    return updatedAt;
-  }
-
-  public void setUpdatedAt(Timestamp updatedAt) {
-    this.updatedAt = updatedAt;
-  }
-
   public Set<HomeDevice> getHomeDevices() {
     return homeDevices;
   }
 
   public void setHomeDevices(Set<HomeDevice> homeDevices) {
     this.homeDevices = homeDevices;
+  }
+
+  @Override
+  public void setCreatedAt(Timestamp createdAt) { this.createdAt = createdAt; }
+
+  public Timestamp getCreatedAt() {
+    return createdAt;
+  }
+
+  @Override
+  public void setUpdatedAt(Timestamp updatedAt) { this.updatedAt = updatedAt; }
+
+  public Timestamp getUpdatedAt() {
+    return updatedAt;
   }
 }

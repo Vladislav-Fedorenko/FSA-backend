555ad42c9973dfbdfb028c45cf42d8dc907e1506
Vladislav Fedorenko <fedorenko.vladislav.k@gmail.com>
2017-12-09 23:16:25 +0600

    Added method for deviceRepository in config of repositories

--- src/main/java/topLevelDomain/secondLevelDomain/homeneeds/config/RepositoriesConfig.java
+++ src/main/java/topLevelDomain/secondLevelDomain/homeneeds/config/RepositoriesConfig.java
@@ -2,6 +2,8 @@ package topLevelDomain.secondLevelDomain.homeneeds.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import topLevelDomain.secondLevelDomain.homeneeds.core.repositories.device.DatabaseDeviceRepository;
+import topLevelDomain.secondLevelDomain.homeneeds.core.repositories.device.DeviceRepository;
 import topLevelDomain.secondLevelDomain.homeneeds.core.repositories.homeDevice.DatabaseHomeDeviceRepository;
 import topLevelDomain.secondLevelDomain.homeneeds.core.repositories.homeDevice.HomeDeviceRepository;
 import topLevelDomain.secondLevelDomain.homeneeds.utils.database.DatabaseTasksExecutorFactory;
@@ -13,5 +15,10 @@ public class RepositoriesConfig {
   @Bean
   public HomeDeviceRepository homeDeviceRepository() {
     return new DatabaseHomeDeviceRepository(factory);
+  }
+
+  @Bean
+  public DeviceRepository deviceRepository() {
+    return new DatabaseDeviceRepository(factory);
   }
 }

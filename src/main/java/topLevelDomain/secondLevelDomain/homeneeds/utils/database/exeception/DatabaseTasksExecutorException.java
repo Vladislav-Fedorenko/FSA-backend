85074881eedcd3af035e136ad8c4937cffb5b63e
Vladislav Fedorenko <fedorenko.vladislav.k@gmail.com>
2018-01-06 18:36:05 +0600

    Constructor by string has been added to DatabaseTasksExecutorException

--- src/main/java/topLevelDomain/secondLevelDomain/homeneeds/utils/database/exeception/DatabaseTasksExecutorException.java
+++ src/main/java/topLevelDomain/secondLevelDomain/homeneeds/utils/database/exeception/DatabaseTasksExecutorException.java
@@ -4,4 +4,8 @@ public class DatabaseTasksExecutorException extends Throwable {
   public DatabaseTasksExecutorException(String s, Throwable throwable) {
     super(s, throwable);
   }
+
+  public DatabaseTasksExecutorException(String s) {
+    super(s);
+  }
 }

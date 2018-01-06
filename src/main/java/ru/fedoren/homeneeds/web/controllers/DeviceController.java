b4629fbfadaea84363c890558bed44e1a92f4181
Vladislav Fedorenko <fedorenko.vladislav.k@gmail.com>
2018-01-06 23:20:37 +0600

    Mistakes of checkstyle have been fixed in device's controller

--- src/main/java/ru/fedoren/homeneeds/web/controllers/DeviceController.java
+++ src/main/java/ru/fedoren/homeneeds/web/controllers/DeviceController.java
@@ -1,11 +1,15 @@
 package ru.fedoren.homeneeds.web.controllers;
 
+import java.net.URI;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.util.UriComponentsBuilder;
-import java.net.URI;
 
 import ru.fedoren.homeneeds.core.models.Device;
 import ru.fedoren.homeneeds.core.repositories.device.DeviceRepository;
@@ -20,32 +24,46 @@ public class DeviceController {
     this.deviceRepository = deviceRepository;
   }
 
+  /**
+   * Create new device.
+   * @param newDevice object for creating
+   * @return created object
+   */
   @RequestMapping(method = RequestMethod.POST)
   @ResponseBody
   public ResponseEntity<Device> create(@RequestBody Device newDevice) {
     Device createdDevice = deviceRepository.addNewDevice(newDevice);
     URI location = UriComponentsBuilder.fromPath("/device/")
-      .path(String.valueOf(createdDevice.getId()))
-      .build().toUri();
+        .path(String.valueOf(createdDevice.getId()))
+        .build().toUri();
     return ResponseEntity.created(location).body(createdDevice);
   }
 
+  /**
+   * Update device.
+   * @param newDevice object for updating
+   * @return updated object
+   */
   @RequestMapping(method = RequestMethod.PUT)
   @ResponseBody
   public ResponseEntity<Device> update(@RequestBody Device newDevice) {
     Device createdDevice = deviceRepository.updateDevice(newDevice);
     URI location = UriComponentsBuilder.fromPath("/device/")
-      .path(String.valueOf(createdDevice.getId()))
-      .build().toUri();
+        .path(String.valueOf(createdDevice.getId()))
+        .build().toUri();
     return ResponseEntity.created(location).body(createdDevice);
   }
 
+  /**
+   * Delete device.
+   * @return result of deleting
+   */
   @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
   public ResponseEntity delete(@PathVariable String id) {
     boolean resultOfDeleting = deviceRepository.deleteDevice(Long.valueOf(id));
     URI location = UriComponentsBuilder.fromPath("/device/")
-      .path(String.valueOf(resultOfDeleting))
-      .build().toUri();
+        .path(String.valueOf(resultOfDeleting))
+        .build().toUri();
     return ResponseEntity.created(location).body(resultOfDeleting);
   }
 }
